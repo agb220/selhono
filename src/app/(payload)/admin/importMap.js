ed544ce0137423997842da660e141b8191419f28
agb220 <annabalabonchik@gmail.com>
2026-07-07 20:25:15 +0300

    update config

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,8 +1,10 @@
 import { default as default_2ee2a43e65847bb43a60a6634170cbbc } from '@/app/(frontend)/_components/Admin/ApprovedBadge'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
 export const importMap = {
   "@/app/(frontend)/_components/Admin/ApprovedBadge#default": default_2ee2a43e65847bb43a60a6634170cbbc,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
